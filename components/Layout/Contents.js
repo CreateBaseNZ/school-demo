f7c1f63dfa6dd590ecfe304592132947cce08c9e
Louis <louiscflin@gmail.com>
2021-05-05 16:53:27 +1200

    Update: design changes

--- components/Layout/Contents.js
+++ components/Layout/Contents.js
@@ -11,6 +11,30 @@ import classes from "./Contents.module.scss";
 
 SwiperCore.use([Pagination, Navigation]);
 
+const DUMMY_SLIDES = [
+  {
+    id: 1234,
+    title: "Slide 1",
+    type: "text",
+    contents:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ex nunc, elementum ac felis et, rhoncus vestibulum sapien. Quisque erat mi, lobortis tincidunt semper vel, scelerisque non sapien. Mauris eget dignissim elit. Proin sed convallis mauris, ut porttitor metus. Maecenas convallis eleifend orci non ornare.",
+  },
+  {
+    id: 1235,
+    title: "Slide 2",
+    type: "text",
+    contents:
+      "Vivamus sagittis diam quis tristique rutrum. Proin at magna risus. Vestibulum eu gravida massa. In viverra libero nec massa bibendum, ultricies faucibus dui consectetur. Curabitur luctus sem lorem, pulvinar facilisis neque faucibus et. Proin accumsan nulla tristique orci suscipit luctus. Phasellus ac elit ut arcu pellentesque pulvinar. Duis dapibus laoreet orci a vestibulum.",
+  },
+  {
+    id: 1236,
+    title: "Slide 3",
+    type: "text",
+    contents:
+      "Mauris faucibus, odio vitae tincidunt euismod, odio lorem vehicula neque, in volutpat eros nisi sit amet tortor. Vivamus in ultricies metus. Donec cursus molestie tortor. Donec interdum dui at urna feugiat, et hendrerit magna semper. Ut libero nulla, tempor quis bibendum vitae, feugiat et augue.",
+  },
+];
+
 const renderBullet = (index, className) => {
   return `<span class="${className}"></span>`;
 };
@@ -42,10 +66,11 @@ const Contents = (props) => {
       <div className={classes.swiperNext}>
         <KeyboardArrowRightIcon style={{ fontSize: 48 }} />
       </div>
-      <SwiperSlide className={classes.swiperSlide}>Slide 1</SwiperSlide>
-      <SwiperSlide className={classes.swiperSlide}>Slide 2</SwiperSlide>
-      <SwiperSlide className={classes.swiperSlide}>Slide 3</SwiperSlide>
-      <SwiperSlide className={classes.swiperSlide}>Slide 4</SwiperSlide>
+      {DUMMY_SLIDES.map((slide) => (
+        <SwiperSlide key={slide.id} className={classes.swiperSlide}>
+          {slide.contents}
+        </SwiperSlide>
+      ))}
       <div className={classes.pagination}></div>
     </Swiper>
   );
