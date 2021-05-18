7193b68ecb6db117c929bea97ff278e0bb489f90
Louis <clin750@aucklanduni.ac.nz>
2021-05-19 00:54:31 +1200

    feat: monaco and workflow comps

---
+++ components/UI/MonacoEditor.js
@@ -0,0 +1,64 @@
+import { useRef } from "react";
+import Editor from "@monaco-editor/react";
+
+import { ServoMotors } from "../Workspace/ServoMotors.ts";
+import { Actuation } from "../Workspace/Actuation.ts";
+import { Block } from "../Workspace/Block";
+
+import themes from "../../utils/themes";
+
+import classes from "./MonacoEditor.module.scss";
+
+const DUMMY_HEADER = "console.log('hello');";
+
+const MonacoEditor = (props) => {
+  const editorRef = useRef();
+  const monacoRef = useRef();
+
+  // const [monacoTheme, setMonacoTheme] = useState("Monokai");
+  // console.log(monacoTheme);
+
+  const clickHandler = async () => {
+    let someVar = props.unityContext;
+    let RoboticSystemName = "RoboticArm";
+    let ServoMotorsClass = ServoMotors;
+    let ActuationClass = Actuation;
+    let sensorData = props.sensorData;
+    // let promise = () => {
+    //   return new Promise((resolve, reject) => {
+    //     someVar.on("GetSensorData", (data) => {
+    //       sensorData = JSON.parse(data);
+    //       resolve();
+    //     });
+    //   });
+    // };
+    // sensorData = await promise();
+    eval("(async () => {" + editorRef.current.getValue() + "})()");
+    monacoRef.current.editor.defineTheme("customTheme", themes["Monokai"]);
+    monacoRef.current.editor.setTheme("customTheme");
+  };
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+  };
+
+  // const themeOptions = Object.keys(themes).map((key) => {
+  //   return <button key={key}> {key} </button>;
+  // });
+
+  return (
+    <div className={classes.editorContainer}>
+      <Editor
+        defaultLanguage="javascript"
+        defaultValue="// Start coding!"
+        onMount={handleEditorDidMount}
+        className={classes.editor}
+        theme={"vs-dark"}
+      />
+      <button onClick={clickHandler}>Click me</button>
+    </div>
+  );
+};
+
+export default MonacoEditor;
--- components/Workspace/Workspace.js
+++ components/Workspace/Workspace.js
@@ -1,65 +1,16 @@
-import Editor from "@monaco-editor/react";
-
-import { useRef } from "react";
-import themes from "../../utils/themes";
-
-import { ServoMotors } from "./ServoMotors.ts";
-import { Actuation } from "./Actuation.ts";
-import { Block } from "./Block";
+import FlowEditor from "../UI/FlowEditor";
+import MonacoEditor from "../UI/MonacoEditor";
 
 import classes from "./Workspace.module.scss";
 
-const DUMMY_HEADER = "console.log('hello');";
-
 const Workspace = (props) => {
-  const editorRef = useRef();
-  const monacoRef = useRef();
-  // const [monacoTheme, setMonacoTheme] = useState("Monokai");
-  // console.log(monacoTheme);
-
-  const handleEditorDidMount = (editor, monaco) => {
-    editorRef.current = editor;
-    monacoRef.current = monaco;
-  };
-
-  const clickHandler = async () => {
-    let someVar = props.unityContext;
-    let RoboticSystemName = "RoboticArm";
-    let ServoMotorsClass = ServoMotors;
-    let ActuationClass = Actuation;
-    let sensorData = props.sensorData;
-    // let promise = () => {
-    //   return new Promise((resolve, reject) => {
-    //     someVar.on("GetSensorData", (data) => {
-    //       sensorData = JSON.parse(data);
-    //       resolve();
-    //     });
-    //   });
-    // };
-    // sensorData = await promise();
-    eval("(async () => {" + editorRef.current.getValue() + "})()");
-    monacoRef.current.editor.defineTheme("customTheme", themes["Monokai"]);
-    monacoRef.current.editor.setTheme("customTheme");
-  };
-
-  const themeOptions = Object.keys(themes).map((key) => {
-    return <button key={key}> {key} </button>;
-  });
-
   return (
     <div className={classes.workspace}>
-      <Editor
-        defaultLanguage="javascript"
-        defaultValue="// Start coding!"
-        onMount={handleEditorDidMount}
-        className={classes.editor}
-        theme={"vs-dark"}
+      <MonacoEditor
+        unityContext={props.unityContext}
+        sensorData={props.sensorData}
       />
-      <button onClick={clickHandler}> Click me </button>{" "}
-      <div className={classes.dropdown}>
-        <button className={classes.dropdownBtn}> Click me </button>{" "}
-        <div className={classes.dropdownMenu}> {themeOptions} </div>{" "}
-      </div>
+      <FlowEditor />
     </div>
   );
 };
