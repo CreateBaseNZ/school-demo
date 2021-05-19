a14798e4c835752f99c339b312efce989713dee7
Louis <clin750@aucklanduni.ac.nz>
2021-05-19 17:27:24 +1200

    feat: dnd node styles

--- components/UI/FlowEditor/DndBar.js
+++ components/UI/FlowEditor/DndBar.js
@@ -1,3 +1,8 @@
+import OpenWithRoundedIcon from "@material-ui/icons/OpenWithRounded";
+import MemoryIcon from "@material-ui/icons/Memory";
+import SettingsInputHdmiOutlinedIcon from "@material-ui/icons/SettingsInputHdmiOutlined";
+import PauseCircleOutlineOutlinedIcon from "@material-ui/icons/PauseCircleOutlineOutlined";
+
 import classes from "./DndBar.module.scss";
 
 const DndBar = () => {
@@ -14,29 +19,94 @@ const DndBar = () => {
 
   return (
     <aside className={classes.wrapper}>
-      <div className="description">
-        You can drag these nodes to the pane on the right.
-      </div>
+      <div className={classes.description}>Drag &amp; Drop</div>
       <div
-        className={`${classes.dndnode} ${classes.input}`}
+        className={`${classes.dndNode} ${classes.move}`}
         onDragStart={(event) => onDragStart(event, "input")}
         draggable
       >
-        Input Node
+        <h5>
+          <OpenWithRoundedIcon />
+          Move Motor
+        </h5>
+        <div className={classes.inputContainer}>
+          <div className={classes.inputWrapper}>
+            <label for="move-x">X</label>
+            <input type="number" id="move-x" placeholder="0" />
+          </div>
+          <div className={classes.inputWrapper}>
+            <label for="move-y">Y</label>
+            <input type="number" id="move-y" placeholder="0" />
+          </div>
+          <div className={classes.inputWrapper}>
+            <label for="move-z">Z</label>
+            <input type="number" id="move-z" placeholder="0" />
+          </div>
+        </div>
       </div>
       <div
-        className={classes.dndnode}
+        className={`${classes.dndNode} ${classes.read}`}
         onDragStart={(event) => onDragStart(event, "default")}
         draggable
       >
-        Default Node
+        <h5>
+          <MemoryIcon />
+          Read Encoder
+        </h5>
+        <div className={classes.selectWrapper}>
+          <select name="read-motor" id="read-motor" required>
+            <option value="" disabled selected hidden>
+              Select a motor
+            </option>
+            <option value="Motor A">Motor A</option>
+            <option value="Motor B">Motor B</option>
+            <option value="Motor C">Motor C</option>
+          </select>
+        </div>
       </div>
       <div
-        className={`${classes.dndnode} ${classes.output}`}
+        className={`${classes.dndNode} ${classes.set}`}
         onDragStart={(event) => onDragStart(event, "output")}
         draggable
       >
-        Output Node
+        <h5>
+          <SettingsInputHdmiOutlinedIcon />
+          Set Up Arm
+        </h5>
+        <div className={classes.selectWrapper}>
+          <select name="set-motor" id="set-motor" required>
+            <option value="" disabled selected hidden>
+              Select a motor
+            </option>
+            <option value="Motor A">Motor A</option>
+            <option value="Motor B">Motor B</option>
+            <option value="Motor C">Motor C</option>
+          </select>
+        </div>
+      </div>
+      <div
+        className={`${classes.dndNode} ${classes.pause}`}
+        onDragStart={(event) => onDragStart(event, "output")}
+        draggable
+      >
+        <h5>
+          <PauseCircleOutlineOutlinedIcon />
+          Pause
+        </h5>
+        <div className={classes.selectAndInput}>
+          <div className={classes.inputWrapper}>
+            <input type="number" id="pause-duration" placeholder="0" />
+          </div>
+          <div className={classes.selectWrapper}>
+            <select name="pause-unit" id="pause-unit" required>
+              <option value="" disabled selected hidden>
+                Unit
+              </option>
+              <option value="ms">ms</option>
+              <option value="s">s</option>
+            </select>
+          </div>
+        </div>
       </div>
     </aside>
   );
