b9dfcb7a40aa5ed7fd487bef258357179fc59a94
Brydon <bbur543@aucklanduni.ac.nz>
2021-05-27 10:54:45 +1200

    Guide Styling

--- components/Contents/Contents.js
+++ components/Contents/Contents.js
@@ -21,7 +21,7 @@ const DUMMY_DATA = [
       title: "Slide 1",
       type: "text",
       contents:
-        "To complete the subsystem, you have to collect all of the floating gems by moving the collector at the robotic arm’s end through each of their locations.",
+        "In this subsystem, you will learn how to use the <span style=\"font-weight: 700;\">Move Motor</span> code block to move the end of a robotic arm to specific x, y, z coordinates. To complete the subsystem, you have to collect all of the floating gems by moving the gem collector through each of their locations.",
     },
     {
       id: "sub1-page2",
@@ -35,21 +35,35 @@ const DUMMY_DATA = [
       title: "Slide 3",
       type: "text",
       contents:
-        "You can program the robotic arm to move to a specific location by placing a Move Motor block and entering the desired coordinates. To run your code, you will need to connect it to the start and end blocks, then press run. Connecting blocks together is as simple as dragging from the output handle of one block to the input handle of another.",
+        "You can program the robotic arm to move to a specific location by placing a <span style=\"font-weight: 700;\">Move Motor</span> block into the workspace and entering the target coordinates.",
     },
     {
       id: "sub1-page4",
       title: "Slide 4",
       type: "text",
       contents:
-        "To make the end of the robotic arm travel to multiple locations, you will need to place a sequence of Move Motor blocks into the workspace and connect them together in your desired order.",
+        "To run your code, you will need to make sure that all of your blocks are connected together with a single path, including the start and end blocks. Connecting blocks together is as simple as dragging from the output handle of one block to the input handle of another.",
     },
     {
       id: "sub1-page5",
       title: "Slide 5",
       type: "text",
       contents:
-        "Once you have connected each of your blocks in the correct order (don’t forget to connect to the start and end blocks), hit Verify My Code on the next slide to check your code answer. Note: If you want to run some code you are testing without having your answer checked, then use the Play button instead.",
+        "Once all of your blocks in the workspace are connected via a single path, press the <span style=\"font-weight: 700; color: #18dbac;\">Play</span> button to reset the simulation and run your code.",
+    },
+    {
+      id: "sub1-page6",
+      title: "Slide 6",
+      type: "text",
+      contents:
+        "To make the end of the robotic arm travel to multiple locations, you will need to place a sequence of <span style=\"font-weight: 700;\">Move Motor</span> blocks into the workspace and connect them together in your desired order.",
+    },
+    {
+      id: "sub1-page7",
+      title: "Slide 7",
+      type: "text",
+      contents:
+        "Once you have connected each of your blocks in the correct order (don’t forget to connect to the start and end blocks), hit <span style=\"font-weight: 700; color: #9063f1;\">Verify my code!</span> on the next slide to check your code answer.",
     },
   ],
 
@@ -66,21 +80,21 @@ const DUMMY_DATA = [
       title: "Slide 2",
       type: "text",
       contents:
-        "The arm will automatically move into the correct position above the bin, you only need to control the claw. By default, if you add an open claw block to the workspace and then click run, the claw will open as soon as the code is run. This will cause it to drop the rubbish too early.",
+        "The arm will automatically move into the correct position above the bin, you only need to control the claw. By default, if you add a <span style=\"font-weight: 700;\">Toggle Claw</span> block to the workspace and then click <span style=\"font-weight: 700; color: #18dbac;\">Play</span>, the claw will open as soon as the code is run. This will cause it to drop the rubbish too early.",
     },
     {
       id: "sub1-page3",
       title: "Slide 3",
       type: "text",
       contents:
-        "You will need to use a Delay block followed by a <span style={{fontWeight: 700, fontSize: 12}}>Toggle Claw</span> block to make the claw open only once it is in the correct position. Using trial-and-error, you will need to find the correct time to wait.",
+        "You will need to use a <span style=\"font-weight: 700;\">Delay</span> block followed by a <span style=\"font-weight: 700;\">Toggle Claw</span> block to make the claw open only once it is in the correct position. Using trial-and-error, you will need to find the correct time to wait.",
     },
     {
       id: "sub1-page4",
       title: "Slide 4",
       type: "text",
       contents:
-        "When you have connected each of your blocks together in the correct order, hit Verify My Code on the next slide to check your code answer. Note: If you want to run some code you are testing without having your answer checked, then use the Play button instead.",
+        "When you have connected each of your blocks together in the correct order, press <span style=\"font-weight: 700; color: #9063f1;\">Verify my code!</span> on the next slide to check your code answer. Note: If you want to run some code you are testing without having your answer checked, then use the <span style=\"font-weight: 700; color: #18dbac;\">Play</span> button instead.",
     },
   ],
 
@@ -125,7 +139,7 @@ const DUMMY_DATA = [
       title: "Slide 6",
       type: "text",
       contents:
-        "When you have connected each of your blocks together in the correct order, hit Verify My Code on the next slide to check your final answer. ",
+        "When you have connected each of your blocks together in the correct order, press <span style=\"font-weight: 700; color: #9063f1;\">Verify my code!</span> on the next slide to check your final answer. ",
     },
   ],
 ];
@@ -168,14 +182,14 @@ const Contents = (props) => {
           dangerouslySetInnerHTML={{ __html: slide.contents }}
         ></SwiperSlide>
       ))}
-      <SwiperSlide className={classes.swiperSlide}>
+      {/* <SwiperSlide className={classes.swiperSlide}>
         When you have connected each of your blocks together in the correct
         order, go to the next slide and hit verify to check your code answer!
-      </SwiperSlide>
+      </SwiperSlide> */}
       <SwiperSlide className={`${classes.swiperSlide} ${classes.checkSlide}`}>
         <button>
           <SlowMotionVideoIcon fontSize="large" />
-          Verify your code!
+          Verify my code!
         </button>
       </SwiperSlide>
       <div className={classes.pagination}></div>
