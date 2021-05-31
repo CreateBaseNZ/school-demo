724f1761a7b78c89c5f63b1e4647afb1aff3f48f
Brydon <bbur543@aucklanduni.ac.nz>
2021-06-01 00:42:00 +1200

    Updated for gravity sphere

--- components/Menu/DefineCard.js
+++ components/Menu/DefineCard.js
@@ -13,28 +13,30 @@ const DefineCard = () => {
         spilt bags of rubbish all over a factory floor! Luckily, there are a pair 
         of bins at the edge of the room that we can deposit the rubbish into. 
         However, rather than cleaning it up ourselves, what if we program the 
-        robotic arm to do it for us?
+        H.E.R.&#216; robotic arm to do it for us?
         </p>
       </section>
       <section>
       <p style={{ fontWeight: 600 }}>
       <br/>
-          If we are going to solve our problem with the robotic arm,
+          If we are going to solve our problem with the H.E.R.&#216; robotic arm,
           we will first need to understand its capabilities:
         </p>
         <ol>
           <li>
-            There is a claw attachment on the end of the arm that can be opened
-            and closed. It could be used to grasp the items of rubbish.
+            There is a gravity sphere attachment on the end of the arm (don't ask us 
+            how it works) that can be activated and deactivated. It could be turned on 
+            to attract the items of rubbish and turned off to drop them into a bin.
           </li>
           <li>
-            The arm has five motors, four to move the arm around the space and
-            one to open and close the claw. We could control each motor individually
-            or instead use a pre-made function to move them together.
+            The arm has four motors to move it around in 3D space. We could control 
+            each motor individually or, instead, use a pre-made function to move them 
+            together.
           </li>
           <li>
-            We can’t move this robotic arm and open/close its claw at the same time. 
-            We will have to do one after the other.
+            Because of the way it needs to be programmed, we can’t move this robotic 
+            arm and activate/deactivate its gravity sphere at the same time. We will 
+            have to do one after the other.
           </li>
         </ol>
       </section>
