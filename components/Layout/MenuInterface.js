b6d0398f54b1e41bfa826659a9fbb7d823c6447a
Louis <clin750@aucklanduni.ac.nz>
2021-06-02 23:15:13 +1200

    feat: situation video

--- components/Layout/MenuInterface.js
+++ components/Layout/MenuInterface.js
@@ -112,7 +112,10 @@ const MenuInterface = () => {
         </div>
       </div>
       <div className={classes.rightArea}>
-        {/* <Simulation unityContext={unityContext} sensorData={sensorData} /> */}
+        <video width="100%" height="100%" controls>
+          <source src="/situation.mp4" type="video/mp4" />
+          <source src="/situation.ogg" type="video/ogg" />
+        </video>
       </div>
     </div>
   );
