d5646cb411fae1bdb6ac9421b1188d4119cef553
Brydon <bbur543@aucklanduni.ac.nz>
2021-06-02 16:19:57 +1200

    Almost finished 1 and 2

--- components/Play/Contents/Contents.js
+++ components/Play/Contents/Contents.js
@@ -19,88 +19,112 @@ const DUMMY_DATA = [
       title: "Slide 1",
       type: "text",
       contents:
-        "In this subsystem, the claw attachment is holding another gem! You will need to drop it into the collection bin by opening the claw when it moves into position. Be careful though, the bin won’t stay in the same place for long.",
+        "In this subsystem, the gravity sphere is activated and is holding a valuable gem! You will need to safely drop it into the collection bin by deactivating the gravity sphere when it moves into position. Be careful though, the bin won’t stay in the same place for long!",
     },
     {
       id: "sub1-page2",
       title: "Slide 2",
       type: "text",
       contents:
-        'The arm will automatically move into the correct position above the bin, you only need to control the claw. By default, if you add a <span style="font-weight: 700;">Toggle Claw</span> block to the workspace and then click <span style="font-weight: 700; color: #18dbac;">Play</span>, the claw will open as soon as the code is run. This will cause it to drop the rubbish too early.',
+        'The arm will automatically move into the correct position above the bin at the start of each simulation. You need to deactivate the gravity sphere by adding a <span style="font-weight: 700;">Gravity Switch</span> block to the workspace. Select the Off option on the block to turn off the gravity sphere and release the gem.',
     },
     {
       id: "sub1-page3",
       title: "Slide 3",
       type: "text",
       contents:
-        'You will need to use a <span style="font-weight: 700;">Delay</span> block followed by a <span style="font-weight: 700;">Toggle Claw</span> block to make the claw open only once it is in the correct position. Using trial-and-error, you will need to find the correct time to wait.',
+        'To run your code, you will need to make sure that all of your blocks are connected together with a single track, including the <span style="font-weight: 700; color: #18dbac;">start</span> and <span style="font-weight: 700; color: #fa6f6f;">end</span> blocks. Connecting blocks together is as simple as dragging from the output handle (filled in dot) of one block to the input handle (hollow dot) of another.',
     },
     {
       id: "sub1-page4",
       title: "Slide 4",
       type: "text",
       contents:
-        'When you have connected each of your blocks together in the correct order, press <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> on the next slide to check your code answer. Note: If you want to run some code you are testing without having your answer checked, then use the <span style="font-weight: 700; color: #18dbac;">Play</span> button instead.',
-    },
-  ],
-  [
-    {
-      id: "sub1-page1",
-      title: "Slide 1",
-      type: "text",
-      contents:
-        'In this subsystem, you will learn how to use the <span style="font-weight: 700;">Move Motor</span> code block to move the end of a robotic arm to specific x, y, z coordinates.',
-    },
-    {
-      id: "sub1-page2",
-      title: "Slide 2",
-      type: "text",
-      contents:
-        "To complete the subsystem, you have to collect all of the floating gems by moving the gem collector through each of their locations.",
-    },
-    {
-      id: "sub1-page3",
-      title: "Slide 3",
-      type: "text",
-      contents:
-        'Place your cursor over each gem to reveal the location of the centre of that gem. The first number is the x coordinate (distance along the <span style="font-weight: 700; color: red;">x-axis</span>), followed by the y coordinate (distance along the <span style="font-weight: 700; color: green;">y-axis</span>). The last number is the z coordinate (distance along the <span style="font-weight: 700; color: blue;">z-axis</span>).',
-    },
-    {
-      id: "sub1-page4",
-      title: "Slide 4",
-      type: "text",
-      contents:
-        'You can program the robotic arm to move to a specific location by placing a <span style="font-weight: 700;">Move Motor</span> block into the workspace and entering the target coordinates.',
+        'If a block is connected with multiple tracks, you may get unexpected behaviour when you run your code. To delete a track, select it by clicking on the arrow head and then press the backspace key.',
     },
     {
       id: "sub1-page5",
       title: "Slide 5",
       type: "text",
       contents:
-        "To run your code, you will need to make sure that all of your blocks are connected together with a single path, including the start and end blocks. Connecting blocks together is as simple as dragging from the output handle of one block to the input handle of another.",
+        'When you have connected each of your blocks together in the correct order, press <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> on the next slide to check your code answer. <span style="font-style: italic;">Note: If you want to run some code you are testing without having your answer checked, then use the</span> <span style="font-weight: 700; color: #18dbac;">Play</span> <span style="font-style: italic;">button instead.</span>',
     },
     {
       id: "sub1-page6",
       title: "Slide 6",
       type: "text",
       contents:
-        'Once all of your blocks in the workspace are connected via a single path, press the <span style="font-weight: 700; color: #18dbac;">Play</span> button to reset the simulation and run your code.',
+        'When you have connected each of your blocks together in the correct order, press <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> on the next slide to check your code answer. <span style="font-style: italic;">Note: If you want to run some code you are testing without having your answer checked, then use the</span> <span style="font-weight: 700; color: #18dbac;">Play</span> <span style="font-style: italic;">button instead.</span>',
+    },
+  ],
+
+
+  [
+    {
+      id: "sub1-page1",
+      title: "Slide 1",
+      type: "text",
+      contents:
+        'In this subsystem, you will learn how to use the <span style="font-weight: 700;">Move Arm</span> code block to move the end of the H.E.R.&#216; robotic arm to specific x, y, z coordinates.',
+    },
+    {
+      id: "sub1-page2",
+      title: "Slide 2",
+      type: "text",
+      contents:
+        "To complete this subsystem, you will need to collect all of the floating gems by moving the gravity sphere through each of their locations.",
+    },
+    {
+      id: "sub1-page3",
+      title: "Slide 3",
+      type: "text",
+      contents:
+        'Place your cursor over each gem to reveal the location of the centre of that gem. The first number is the x coordinate (distance along the <span style="font-weight: 700; color: #ff2f00;">x-axis</span>), followed by the y coordinate (distance along the <span style="font-weight: 700; color: #00dc00;">y-axis</span>). The last number is the z coordinate (distance along the <span style="font-weight: 700; color: #007eff;">z-axis</span>).',
+    },
+    {
+      id: "sub1-page4",
+      title: "Slide 4",
+      type: "text",
+      contents:
+        'For reference, the base of the robotic arm is at the coordinates (0, 0, 0). When rotating the camera so that the arm is facing the right: <span style="font-weight: 700; color: #ff2f00;">positive x</span> is right and <span style="font-weight: 700; color: #ff2f00;">negative x</span> is left, <span style="font-weight: 700; color: #00dc00;">positive y</span> is up and <span style="font-weight: 700; color: #00dc00;">negative y</span> is down, <span style="font-weight: 700; color: #007eff;">positive z</span> is into the screen, <span style="font-weight: 700; color: #007eff;">negative z</span> is out of the screen.',
+    },
+    {
+      id: "sub1-page5",
+      title: "Slide 5",
+      type: "text",
+      contents:
+        'For example, lets imagine an item at the coordinates (-1, 2, 3). Using the same camera orientation in the previous slide, that object is 1 meter to the left, 2 meters up and 3 meters into the screen compared to the base of the arm at (0,0,0)',
+    },
+    {
+      id: "sub1-page6",
+      title: "Slide 6",
+      type: "text",
+      contents:
+        'You can program the robotic arm to move to a specific location by dragging a <span style="font-weight: 700;">Move Arm</span> block into the workspace and entering the target coordinates.',
     },
     {
       id: "sub1-page7",
       title: "Slide 7",
       type: "text",
       contents:
-        'To make the end of the robotic arm travel to multiple locations, you will need to place a sequence of <span style="font-weight: 700;">Move Motor</span> blocks into the workspace and connect them together in your desired order.',
+        'When using the <span style="font-weight: 700;">Move Arm</span> block, the coordinates that you enter will be the location of the centre of the gravity sphere when the arm has finished moving.',
     },
     {
       id: "sub1-page8",
       title: "Slide 8",
       type: "text",
       contents:
-        'Once you have connected each of your blocks in the correct order (don’t forget to connect to the start and end blocks), hit <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> on the next slide to check your code answer.',
+        'Once all of your blocks in the workspace are connected via a single track (don’t forget to connect to the <span style="font-weight: 700; color: #18dbac;">start</span> and <span style="font-weight: 700; color: #fa6f6f;">end</span> blocks), press the <span style="font-weight: 700; color: #18dbac;">Play</span> button to reset the simulation and test your current code.',
+    },
+    {
+      id: "sub1-page9",
+      title: "Slide 9",
+      type: "text",
+      contents:
+        'When you think that you have the correct code, press the <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> button on the next slide to confirm your answer and complete the task!',
     },
   ],
+
 
   [
     {
@@ -122,7 +146,7 @@ const DUMMY_DATA = [
       title: "Slide 3",
       type: "text",
       contents:
-        'When using the <span style="font-weight: 700;">Move Motor</span> block, the coordinates that you enter will be the location of the centre of the gravity sphere attachment when the block has finished running.',
+        'To make the end of the robotic arm travel to multiple locations, you will need to place a sequence of <span style="font-weight: 700;">Move Motor</span> blocks into the workspace and connect them together in your desired order.',
     },
     {
       id: "sub1-page4",
@@ -139,15 +163,15 @@ const DUMMY_DATA = [
         "For example, you may have to lift each captured rubbish bag vertically before moving it to the rubbish bin to make sure that you do not hit the side of the bin or other rubbish bags.",
     },
     {
-      id: "sub1-page5",
-      title: "Slide 5",
+      id: "sub1-page6",
+      title: "Slide 6",
       type: "text",
       contents:
         'To keep it simple, try adding blocks to deposit a single rubbish bag into the bin, then use the <span style="font-weight: 700; color: #18dbac;">Play</span> button to test your code. If successful, keep adding more blocks until you can deposit every bag in one run.',
     },
     {
-      id: "sub1-page6",
-      title: "Slide 6",
+      id: "sub1-page7",
+      title: "Slide 7",
       type: "text",
       contents:
         'When you have connected each of your blocks together in the correct order, press <span style="font-weight: 700; color: #9063f1;">Verify my code!</span> on the next slide to check your final answer. ',
