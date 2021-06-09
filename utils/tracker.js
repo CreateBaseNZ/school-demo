4fcb9088ed4e71841b5eb7344ddb0bcdc7e8d97e
Carl Velasco <carlvelasco96@gmail.com>
2021-06-10 11:48:46 +1200

    First 5 tracking details. Progress with the tracking tool.

---
+++ utils/tracker.js
@@ -0,0 +1,28 @@
+import trackings from "../public/data/trackings.json";
+import axios from "axios";
+
+const send = (behaviour) => {
+  return new Promise(async (resolve, reject) => {
+    
+  });
+};
+
+const click = (code, properties) => {
+  return new Promise(async (resolve, reject) => {
+    // Declare variables
+
+    // Fetch the tracking
+
+    // Validate properties
+
+    // Create the behaviour
+
+    // Store the behaviour
+
+  });
+};
+
+export const tracker = {
+  send,
+  click,
+};
