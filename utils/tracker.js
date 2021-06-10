e6d402a5cedb73ece63ee0ccbb28b30692be43c1
Carl Velasco <carlvelasco96@gmail.com>
2021-06-10 16:36:15 +1200

    Completed tracking click tool

--- utils/tracker.js
+++ utils/tracker.js
@@ -3,23 +3,45 @@ import axios from "axios";
 
 const send = (behaviour) => {
   return new Promise(async (resolve, reject) => {
-    
+    // Fetch the current date
+    const date = new Date().toString();
+    // Send the behaviour
+    let data;
+    try {
+      data = (
+        await axios.post("/api/cookie/add-behaviour", { date, behaviour })
+      )["data"];
+    } catch (error) {
+      return reject({ status: "succeeded", content: error });
+    }
+    // Handlers
+    switch (data.status) {
+      case "failed":
+        return reject(data);
+      case "error":
+        return reject(data);
+      default:
+        return resolve(data.content);
+    }
   });
 };
 
 const click = (code, properties) => {
   return new Promise(async (resolve, reject) => {
     // Declare variables
-    properties.date = new Date().toString();
+    properties.date = date;
     // Fetch the tracking
-    const tracking = trackings.find(element => element.code === code);
+    const tracking = trackings.find((element) => element.code === code);
     // Validate properties and create the behaviour
     let behaviour = new Object({ code });
     for (const property in tracking.properties) {
       if (!properties[property]) {
-        return reject({ status: "failed", content: `You are missing '${property}' property.` });
+        return reject({
+          status: "failed",
+          content: `You are missing '${property}' property.`,
+        });
       } else {
-        behaviour[property] = properties[property]
+        behaviour[property] = properties[property];
       }
     }
     // Store the behaviour
@@ -32,5 +54,5 @@ const click = (code, properties) => {
 };
 
 export const tracker = {
-  click
+  click,
 };
