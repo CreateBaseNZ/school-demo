192bcc08750b89ed4931394080d9427e04f392ee
Carl Velasco <carlvelasco96@gmail.com>
2021-06-10 14:18:24 +1200

    Implemented Click Tracker

--- utils/tracker.js
+++ utils/tracker.js
@@ -10,19 +10,27 @@ const send = (behaviour) => {
 const click = (code, properties) => {
   return new Promise(async (resolve, reject) => {
     // Declare variables
-
+    properties.date = new Date().toString();
     // Fetch the tracking
-
-    // Validate properties
-
-    // Create the behaviour
-
+    const tracking = trackings.find(element => element.code === code);
+    // Validate properties and create the behaviour
+    let behaviour = new Object({ code });
+    for (const property in tracking.properties) {
+      if (!properties[property]) {
+        return reject({ status: "failed", content: `You are missing '${property}' property.` });
+      } else {
+        behaviour[property] = properties[property]
+      }
+    }
     // Store the behaviour
-
+    try {
+      await send(behaviour);
+    } catch (data) {
+      return reject(data);
+    }
   });
 };
 
 export const tracker = {
-  send,
-  click,
+  click
 };
